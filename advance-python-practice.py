f1445b99cba3fac576f5abc7eb79e02f173cc82a
rezaazimibar <www.beta04@gmail.com>
2024-06-24 17:55:08 +0330

    add new practice file

---
+++ advance-python-practice.py
@@ -0,0 +1,17 @@
+#--------------------------------conditional-logic------------------------------------
+# is_old=False
+# is_young=True
+
+# if is_old:
+#     print("yes you can drive!!!")
+# elif is_young:
+#     print("you can run it")
+# else:
+#     print("you can't drive")
+
+#-------------------------------ternary-operator---------------------------------------
+
+is_that_myfreind=True
+ternary_value="yes his my freind"if is_that_myfreind else "no he is not my freind"
+
+print(f"is he my freind: {ternary_value}")
