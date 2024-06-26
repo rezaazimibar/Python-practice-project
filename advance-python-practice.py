2f20eb2fdb3d3efd1a0245231cafaeae41470acf
rezaazimibar <www.beta04@gmail.com>
2024-06-26 13:21:23 +0330

    advance practice part 2

--- advance-python-practice.py
+++ advance-python-practice.py
@@ -50,11 +50,67 @@
 
 #------------------------------------------for-loop-------------------------------------
 
-for item in "hi my name is reza":
-    print(item)
-for set_item in (1,3,4,5,6,7):
-    print(set_item)
+# for item in "hi my name is reza":
+#     print(item)
 
-for tuple_item in {3,5,1,6,7}:
-    for list_item in ["hi",True,343,"my god"]:
-        print(f"inner loop: {tuple_item,list_item}")
+# for set_item in (1,3,4,5,6,7):
+#     print(set_item)
+
+# for tuple_item in {3,5,1,6,7}:
+#     for list_item in ["hi",True,343,"my god"]:
+#         print(f"inner loop: {tuple_item,list_item}")
+#         print(tuple_item,list_item)
+
+#--------------------------------------iterable-----------------------------
+
+# #iterable can be set list dictionary tuple string
+# #it mean you looping over the something
+# myDict={"name":"reza","age":34,"single":True}
+
+# for key,value in myDict.items():
+#     # key,value=item; instead we can modify up
+#     print(key,value)
+
+# for item in myDict.values(): #we can use key to show key
+#     print(item)
+#----------------------------------------exercise--------------------------------
+
+# my_list=[1,2,3,4,5]
+
+# total=0
+# for item in my_list:
+#     total=total +item
+#     print(total)
+
+# print(total)
+
+#-----------------------------------------range-()----------------------------------
+
+# for item in range(0,100,11):
+#     print(item)
+
+# for rangeItem in range(100,0,-7):
+#     print(set(range(4)))
+
+#-----------------------------------------enumerate-()--------------------------------
+
+# for i,item in enumerate("hi reza how are you man"):
+#     print(i,item)
+
+# #fun exercise
+# for i, item in enumerate(list(range(100))):
+#     if item ==50:
+#         print(f"the index of fifty is: {i}")
+
+#------------------------------------------while-loop-----------------------------------
+
+i=0
+while i<50:
+    print(i)
+    i +=1
+else:
+    print("job is done")
+
+while True:
+    print("infinite loop bread")
+    break       #so important if it is not we have infinite loop that dangerous
